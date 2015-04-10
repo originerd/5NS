3679e1810a010daf414565e960d5337d32e0e2ce
Jitae Kim <originerd@gmail.com>
2015-04-10 23:50:01 +0900

    Modified squeeze_content method

--- app/models/nanopost.rb
+++ app/models/nanopost.rb
@@ -36,6 +36,6 @@ class Nanopost < ActiveRecord::Base
   # Squeeze '!? '
   def squeeze_content
     content.gsub!(/[?! ]{2,}/) { |match| match.split('').uniq.join }
-    content.chomp!
+    content.strip!
   end
 end
